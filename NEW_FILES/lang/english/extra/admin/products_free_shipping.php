0b1772a15e09bd57967db794d936bb5eab78447b
markus-willems <markus.willems@googlemail.com>
2016-12-14 15:25:43 +0100

    Fixed wrong description

--- NEW_FILES/lang/english/extra/admin/products_free_shipping.php
+++ NEW_FILES/lang/english/extra/admin/products_free_shipping.php
@@ -1,15 +1,15 @@
 <?php
 
-define("HEADING_PRODUCTS_FREE_SHIPPING", "Free shipping options"
+define("HEADING_PRODUCTS_FREE_SHIPPING", "Free shipping options");
 
 define("TEXT_PRODUCTS_FREE_SHIPPING", "Free shipping allowed?:");
 define("TEXT_PRODUCTS_FREE_SHIPPING_INFO", "Yes = Free shipping; No = No free shipping");
 
-define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_1", "Max. amount in shopping cart (free shipping):");
-define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_1_INFO", "Maximum amount that can be ordered with free shipping");
+define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_1", "Max. cart amount:");
+define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_1_INFO", "Maximum amount that can be ordered");
 
-define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_2", "Max. amount in shopping cart:");
-define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_2_INFO", "Maximum amount that can be ordered");
+define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_2", "Max. free shipping amount:");
+define("TEXT_PRODUCTS_FREE_SHIPPING_AMOUNT_2_INFO", "Maximum amount that will be available for free shipping (will be ignored if cart amount is set > 0)");
 
 define("TEXT_PRODUCTS_FREE_SHIPPING_COUNTRIES", "Valid for the following countries:");
 define("TEXT_PRODUCTS_FREE_SHIPPING_COUNTRIES_INFO", "Comma-separated country codes (Example: DE,AT,CH)");
